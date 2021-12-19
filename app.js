c7b4add13dd8f171f2c2f28ec31a1e0dede482d2
konmarii <chankonworld@gmail.com>
2021-12-19 13:02:03 +0900

    HTMLクラス

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var app = new Vue({
   el: '#app',
   data: {
-    frameworks: [
-      { name: 'Vue', creator: 'Evan You', stars: 79760 },
-      { name: 'React', creator: 'Facebook', stars: 85623 },
-      { name: 'Angular', creator: 'Google', stars: 31976 },
+    messages: [
+      { text: 'This is normal message.', error: false },
+      { text: 'This is ERROR message!', error: true },
     ]
   }
 })
